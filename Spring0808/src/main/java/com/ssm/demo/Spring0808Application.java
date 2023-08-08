e383d3bbb48b058fd2e5db9f33e8767228a78bfe
cc981 <cc981@192.168.1.114>
2023-08-08 16:24:21 +0900

    Spring0808Application.javaの10行目からコメント「//今日は」を追加しました。（20230808）

--- Spring0808/src/main/java/com/ssm/demo/Spring0808Application.java
+++ Spring0808/src/main/java/com/ssm/demo/Spring0808Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring0808Application {
 
 	//こんにちは
+	//今日は
 	public static void main(String[] args) {
 		SpringApplication.run(Spring0808Application.class, args);
 	}
