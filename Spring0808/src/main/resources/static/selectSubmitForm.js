5fdfd938f7b02688f107edabaa49451a19396ff1
cc981 <cc981@192.168.1.2>
2023-08-29 13:34:21 +0900

    JSの学習メモ

--- Spring0808/src/main/resources/static/selectSubmitForm.js
+++ Spring0808/src/main/resources/static/selectSubmitForm.js
@@ -4,12 +4,23 @@
  * author: Rothschilds MA
  * date: 2023/08/24
  */
+// 该程序代码用于控制选择框表单的提交。
 
-
-function setActionAndSubmitForm(formId, urlPath, value, method=undefined) {
+// 函数参数：
+// - formId：表单的ID属性
+// - urlPath：提交目标的基础URL路径
+// - value：选择的值（选择框中选中的选项的值）
+// - method：表单的HTTP方法（可选）
+function setActionAndSubmitForm(formId, urlPath, value, method = undefined) {
+	// 获取表单元素
 	const form = document.getElementById(formId);
+	// 设置表单的action属性为目标URL（包括选择的值）
 	form.action = urlPath + value;
-	if(method !== undefined) form.method=method;
+	// 设置表单的HTTP方法（如提供则设定，否则保留原有方法）
+	if (method !== undefined) form.method = method;
+	// 提交表单
 	form.submit();
 }
-
+//这段代码用于控制指定选择框的表单提交。
+//根据选择框的值，设置表单的动作和HTTP方法，并进行表单提交。
+//作者和日期信息也包含在内，以便于解释代码的目的和功能。
--- Spring0808/src/main/resources/static/submitForm.js
+++ Spring0808/src/main/resources/static/submitForm.js
@@ -1,27 +1,63 @@
 /**
+ * 表单提交控制
  * 
+ * 这个程序代码用于控制页面中的表单提交操作。
+
+ * 首先，定义了一个包含不同操作类型对应的URL路径的映射对象 urlPaths。
+ * 操作类型包括：
+ * - search：搜索操作
+ * - add：插入数据操作
+ * - add2：提交数据操作
+ * - update2：更新数据操作
+ * - update：更新指定日期的数据操作
+ * - delete：删除员工数据操作
+ * - logout：登出操作
+ * 
+ * 然后，定义了一个获取指定操作类型对应URL路径的函数 getUrlPath。
+ * 当调用 getUrlPath 传入操作类型（键名）时，会返回对应的URL路径，如果找不到则返回空字符串。
+ * 
+ * 最后，定义了一个提交表单的函数 submitForm。
+ * 该函数通过传入表单ID、操作类型和可选的HTTP方法，来控制表单的提交。
+ * 首先，获取表单元素。
+ * 然后，根据操作类型从 urlPaths 获取对应的URL路径，并将其设置为表单的action属性。
+ * 如果提供了HTTP方法，也将其设置为表单的method属性。
+ * 最后，执行表单提交操作。
+ */
+/**
+ * 表单提交控制
+ * 
+ * 该程序代码用于控制表单的提交。
+
+ * author: OpenAI 用户
+ * date: 2023/07/19
  */
 
+// 定义不同操作对应的URL路径
 var urlPaths = {
-	search: '/search',
-	add: '/insert',
-	add2: '/submit',
-	update2: '/update2',
-	update: '/{date}/update(date=*{attendanceDate})',
-	delete: '/deleteEmployees',
-	logout: '/logout'
+	search: '/search',         // 搜索操作
+	add: '/insert',            // 新增操作
+	add2: '/submit',           // 提交操作
+	update2: '/update2',       // 更新操作2
+	update: '/{date}/update(date=*{attendanceDate})',  // 更新操作
+	delete: '/deleteEmployees', // 删除操作
+	logout: '/logout'           // 注销操作
 };
 
-
-// URLパスを取得する関数
+// 获取操作对应的URL路径
 function getUrlPath(key) {
-	return urlPaths[key] || '';
+	return urlPaths[key] || '';  // 如果没有对应的操作路径则返回空字符串
 }
 
+// 表单提交函数
 function submitForm(formId, keyName, method = undefined) {
-	var form = document.getElementById(formId); // フォーム要素を取得
-	form.action = getUrlPath(keyName); // フォームのaction属性にURLを設定
-	if (method !== undefined) form.method = method;    // フォームのmethod属性にHTTPメソッドを設定
-	form.submit(); // フォームを送信
+	var form = document.getElementById(formId); // 获取表单元素
+	form.action = getUrlPath(keyName); // 设置表单的action属性为对应操作的URL路径
+	if (method !== undefined) form.method = method; // 如果提供了HTTP方法则设置表单的method属性
+	form.submit(); // 提交表单
 }
-
+//这段代码用于控制表单的提交行为。
+//通过预先定义的操作与URL路径的映射（urlPaths），可以方便地为不同的操作设置对应的URL路径。
+//getUrlPath函数根据操作关键字获取对应的URL路径。
+//submitForm函数根据表单ID
+//操作关键字以及可选的HTTP方法来设置表单的提交行为，将表单数据提交到相应的URL路径。
+//作者和日期信息也包含在内，以便于解释代码的目的和功能。
